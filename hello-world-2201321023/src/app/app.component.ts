abe103086eb60495d36d65b0e127a5ca74a9abb8
Nikola P. <nikoladevelops@gmail.com>
2024-04-25 23:05:48 +0300

    Finished exercise

--- hello-world-2201321023/src/app/app.component.ts
+++ hello-world-2201321023/src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'hello-world-2201321023';
+  currentSectionVisible:string = "";
+
+  // section - is_visible
+  dict: { [key: string]: boolean } = {
+    'about_me': true,
+    'hobbies': true,
+    'creator': true,
+    'channel': true,
+    'offering': true
+  };
+
+  toggleSection(sectionName:string) {
+    if(this.currentSectionVisible != ""){
+      this.dict[this.currentSectionVisible] = !this.dict[this.currentSectionVisible] // hide previous
+    }
+
+    if(this.currentSectionVisible == sectionName){
+      this.currentSectionVisible = ""
+      return;
+    }
+
+    // show new section
+    this.dict[sectionName] = !this.dict[sectionName]
+    this.currentSectionVisible = sectionName
+  }
 }
